50339cbbe4c7c0dd088bc4a467bdf7e288ad9d12
Ben Smith <benjsmi@us.ibm.com>
2015-11-09 10:16:43 -0500

    Fix port number.

--- idsBuild.sh
+++ idsBuild.sh
@@ -11,7 +11,7 @@ echo Downloading Docker requirements..
 wget http://$BUILD_DOCKER_HOST:8081/dockerneeds.tar -q
 echo Setting up Docker...
 tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
-	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2375" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
+	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2376" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
 
 echo Building the docker image...
 ./docker build -t gameon-webapp .
