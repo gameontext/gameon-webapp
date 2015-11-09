d559d3dd40860c597bde3882452ee39b7912dfcd
Ben Smith <benjsmi@us.ibm.com>
2015-11-09 10:16:11 -0500

    Try with quiet output.

--- idsBuild.sh
+++ idsBuild.sh
@@ -3,13 +3,12 @@
 #
 # This script is only intended to run in the IBM DevOps Services Pipeline Environment.
 #
-set +x
 
 echo Informing slack...
 curl -X 'POST' --silent --data-binary '{"text":"A new build for the web application has started."}' $WEBHOOK > /dev/null
 mkdir dockercfg ; cd dockercfg
 echo Downloading Docker requirements..
-wget http://$BUILD_DOCKER_HOST:8081/dockerneeds.tar
+wget http://$BUILD_DOCKER_HOST:8081/dockerneeds.tar -q
 echo Setting up Docker...
 tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
 	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2375" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
