2b8f93424eb5d3afca336ad95e98f31a56811e4b
Ben Smith <benjsmi@us.ibm.com>
2015-11-12 11:38:16 -0500

    Use the internal softlayer IP and have the firewall route logging through this.

--- startup.sh
+++ startup.sh
@@ -4,4 +4,5 @@ cd /opt/frontend
 echo "Starting the node server"
 node server.js &
 echo Running Logstash Forwarder...
+sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
 cd /opt ; ./forwarder --config ./forwarder.conf
