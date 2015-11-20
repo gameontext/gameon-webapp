7921275789aef4a71f919d2f66c41f2203bee0dc
Ben Smith <benjsmi@us.ibm.com>
2015-11-20 16:19:12 -0500

    Change to an nginx container.

--- startup.sh
+++ startup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd /opt/frontend
-echo "Starting the node server"
-node server.js &
-echo Running Logstash Forwarder...
+echo Starting nginx in the background...
+nginx -c /etc/nginx/nginx.conf
+echo Starting the logstash forwarder...
 sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
 cd /opt ; ./forwarder --config ./forwarder.conf
