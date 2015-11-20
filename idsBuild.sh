7bfe414389ab8043d4b0369deb7f59a695bce0a4
Ben Smith <benjsmi@us.ibm.com>
2015-11-20 13:41:20 -0500

    Use HTTP auth and SSL to gather build requirements.

--- idsBuild.sh
+++ idsBuild.sh
@@ -8,7 +8,7 @@ echo Informing slack...
 curl -X 'POST' --silent --data-binary '{"text":"A new build for the web application has started."}' $WEBHOOK > /dev/null
 mkdir dockercfg ; cd dockercfg
 echo Downloading Docker requirements..
-wget http://$BUILD_DOCKER_HOST:8081/dockerneeds.tar -q
+wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/dockerneeds.tar -q
 echo Setting up Docker...
 tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
 	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2376" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
