6757e6e17faa735d58dc92f36047594f1adcf1c2
Ben Smith <benjsmi@us.ibm.com>
2015-12-02 14:18:56 -0500

    Change to just grabbing that one dependency to use Docker-1.9

--- idsBuild.sh
+++ idsBuild.sh
@@ -17,7 +17,8 @@ mkdir dockercfg ; cd dockercfg
 echo Downloading Docker requirements..
 wget --user=admin --password=$ADMIN_PASSWORD https://$BUILD_DOCKER_HOST:8443/dockerneeds.tar -q
 echo Setting up Docker...
-sudo apt-get update ; sudo apt-get install -y lxc
+wget http://security.ubuntu.com/ubuntu/pool/main/a/apparmor/libapparmor1_2.8.95~2430-0ubuntu5.3_amd64.deb -O libapparmor.deb
+sudo dpkg -i libapparmor.deb
 tar xzf dockerneeds.tar ; mv docker ../ ; cd .. ; chmod +x docker ; \
 	export DOCKER_HOST="tcp://$BUILD_DOCKER_HOST:2376" DOCKER_TLS_VERIFY=1 DOCKER_CONFIG=./dockercfg
 
