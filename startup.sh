cd16b66db756a9bf73fac00825aeae191933dcfe
Ben Smith <benjsmi@us.ibm.com>
2015-12-02 14:07:53 -0500

    Syntax changes if you're in bash.

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ if [ "$LOGGING_DOCKER_HOST" != "" ]; then
   echo Starting nginx in the background...
   nginx -c /etc/nginx/nginx.conf
   echo Starting the logstash forwarder...
-  wget https://admin:$ADMIN_PASSWORD@game-on.org:8443/logstashneeds.tar /opt/logstashneeds.tar
+  wget https://admin:$ADMIN_PASSWORD@game-on.org:8443/logstashneeds.tar -O /opt/logstashneeds.tar
   sed -i s/PLACEHOLDER_LOGHOST/$LOGGING_DOCKER_HOST/g /opt/forwarder.conf
   tar xvzf logstashneeds.tar ; rm logstashneeds.tar
   cd /opt ; chmod +x ./forwarder ; tar xvzf logstashneeds.tar ; rm logstashneeds.tar
