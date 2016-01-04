45610e4b8639bc41154cf8eabb81ae5bfccb6384
Ben Smith <benjsmi@us.ibm.com>
2016-01-04 10:33:35 -0500

    Use etcd.

--- startup.sh
+++ startup.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-if [ "$LOGSTASH_ENDPOINT" != "" ]; then
+if [ "$ETCDCTL_ENDPOINT" != "" ]; then
+  echo Setting up etcd...
+  wget https://github.com/coreos/etcd/releases/download/v2.2.2/etcd-v2.2.2-linux-amd64.tar.gz -q
+  tar xzf etcd-v2.2.2-linux-amd64.tar.gz etcd-v2.2.2-linux-amd64/etcdctl --strip-components=1
+  rm etcd-v2.2.2-linux-amd64.tar.gz
+  mv etcdctl /usr/local/bin/etcdctl
+
   echo Starting nginx in the background...
   nginx -c /etc/nginx/nginx.conf
   echo Starting the logstash forwarder...
-  sed -i s/PLACEHOLDER_LOGHOST/$LOGSTASH_ENDPOINT/g /opt/forwarder.conf
+  sed -i s/PLACEHOLDER_LOGHOST/$(etcdctl get /logstash/endpoint)/g /opt/forwarder.conf
   cd /opt
   chmod +x ./forwarder
-  echo -e $LOGSTASH_CERT > logstash-forwarder.crt
-  echo -e $LOGSTASH_KEY > logstash-forwarder.key
+  etcdctl get /logstash/cert > logstash-forwarder.crt
+  etcdctl get /logstash/key > logstash-forwarder.key
   sleep 0.5
   ./forwarder --config ./forwarder.conf
 else
