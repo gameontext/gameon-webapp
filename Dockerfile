970c42343fb4031be64a9bef1ec65cf699e7bff0
Erin Schnabel <schnabel@us.ibm.com>
2017-08-25 11:50:29 -0400

    Remove a8, clean nginx (#114)
    * Remove a8, clean nginx
    
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,20 @@
 FROM nginx
 
-MAINTAINER Erin Schnabel <schnabel@us.ibm.com> (@ebullientworks)
+LABEL maintainer="Erin Schnabel <schnabel@us.ibm.com> (@ebullientworks)"
 
 ADD https://download.elastic.co/logstash-forwarder/binaries/logstash-forwarder_linux_amd64 /opt/forwarder
 
 RUN apt-get update && apt-get install -y wget procps \
-  && rm -rf /var/lib/apt/lists/* \
-  && wget -qO- https://github.com/amalgam8/amalgam8/releases/download/v0.4.2/a8sidecar.sh | sh
+  && rm -rf /var/lib/apt/lists/*
 
 RUN wget https://github.com/coreos/etcd/releases/download/v2.2.2/etcd-v2.2.2-linux-amd64.tar.gz -q && \
     tar xzf etcd-v2.2.2-linux-amd64.tar.gz etcd-v2.2.2-linux-amd64/etcdctl --strip-components=1 && \
     rm etcd-v2.2.2-linux-amd64.tar.gz && \
     mv etcdctl /usr/local/bin/etcdctl
 
-COPY ./nginx.conf /etc/nginx/nginx.conf
-COPY ./nginx-a8.conf /etc/nginx/nginx-a8.conf
-COPY ./nginx-nolog.conf /etc/nginx/nginx-nolog.conf
+COPY ./nginx.conf        /etc/nginx/nginx.conf
+COPY ./nginx-common.conf /etc/nginx/nginx-common.conf
+COPY ./nginx-nolog.conf  /etc/nginx/nginx-nolog.conf
 COPY ./startup.sh /opt/startup.sh
 COPY ./forwarder.conf /opt/forwarder.conf
 
@@ -28,4 +27,4 @@ CMD ["/opt/startup.sh"]
 HEALTHCHECK \
   --timeout=10s \
   --start-period=40s \
-  CMD wget -q -O /dev/null http://localhost:8080/
+  CMD wget -q -O /dev/null http://localhost:8080/health
--- startup.sh
+++ startup.sh
@@ -8,11 +8,6 @@ else
   echo "Using local env for any etcd"
 fi
 
-# Configure amalgam8 for this container
-export A8_SERVICE=webapp:v1
-export A8_ENDPOINT_PORT=8080
-export A8_ENDPOINT_TYPE=http
-
 if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   echo Setting up etcd...
   echo "** Testing etcd is accessible"
@@ -29,55 +24,31 @@ if [ "$ETCDCTL_ENDPOINT" != "" ]; then
   echo "etcdctl returned sucessfully, continuing"
 
   export LOGSTASH_ENDPOINT=$(etcdctl get /logstash/endpoint)
-  export A8_REGISTRY_URL=$(etcdctl get /amalgam8/registryUrl)
-  export A8_CONTROLLER_URL=$(etcdctl get /amalgam8/controllerUrl)
-  export A8_CONTROLLER_POLL=$(etcdctl get /amalgam8/controllerPoll)
-  JWT=$(etcdctl get /amalgam8/jwt)  
-
-  # Softlayer needs a logstash endpoint so we set up the server
-  # to run in the background and the primary task is running the
-  # forwarder. In ICS, Liberty is the primary task so we need to
-  # run it in the foreground
-  if [ "$LOGSTASH_ENDPOINT" != "" ]; then
-     echo Starting nginx in the background...
-     if [ -z "$A8_REGISTRY_URL" ]; then 
-       echo Running without a8.
-       #no a8, just run server.
-       nginx -c /etc/nginx/nginx.conf
-     else
-       #a8, configure security, and run via sidecar.
-       if [ ! -z "$JWT" ]; then     
-         echo Running a8 with security.
-         export A8_REGISTRY_TOKEN=$JWT
-         export A8_CONTROLLER_TOKEN=$JWT
-         CONF=nginx-a8.conf
-       else
-         echo Running a8 with no security.
-         CONF=nginx.conf
-       fi
-       a8sidecar --register nginx -c /etc/nginx/$CONF
-    fi    
-    echo Starting the logstash forwarder...
-    sed -i s/PLACEHOLDER_LOGHOST/${LOGSTASH_ENDPOINT}/g /opt/forwarder.conf
-    cd /opt
-    chmod +x ./forwarder
-    etcdctl get /logstash/cert > logstash-forwarder.crt
-    etcdctl get /logstash/key > logstash-forwarder.key
-    sleep 0.5    
-    ./forwarder --config ./forwarder.conf
-  else
-    echo Launching nginx with  logging to standard out
-    nginx -c /etc/nginx/nginx-nolog.conf
-  fi
 else
   #local environment, no logstash to use.
-  echo -n "Checking if bower has run yet.. "
+  echo -n "Making sure application has been built .. "
   if [ ! -d /opt/www/public/bower_components ]; then
-    echo "..bower has not run, running bower" 
-    cd /opt/www && npm install -g bower@1.5.3 && bower install --allow-root
+    echo ".. bower components are missing. The application was not built before packaging."
+    exit 1
   else
-    echo "..bower has run."
-  fi 
+    echo ".. all is well."
+  fi
+fi
+
+if [ "$LOGSTASH_ENDPOINT" != "" ]; then
+  echo Starting nginx in the background...
+  nginx -c /etc/nginx/nginx.conf
+
+  echo Starting the logstash forwarder...
+  sed -i s/PLACEHOLDER_LOGHOST/${LOGSTASH_ENDPOINT}/g /opt/forwarder.conf
+  cd /opt
+  chmod +x ./forwarder
+  etcdctl get /logstash/cert > logstash-forwarder.crt
+  etcdctl get /logstash/key > logstash-forwarder.key
+  sleep 0.5
+  ./forwarder --config ./forwarder.conf
+else
   echo No logging host set. Running nginx to standard out...
+  echo Launching nginx with  logging to standard out
   nginx -c /etc/nginx/nginx-nolog.conf
 fi
