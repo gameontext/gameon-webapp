e804e62f3314de4c287cf794bf9be42e6b481ecf
Erin Schnabel <schnabel@us.ibm.com>
2018-09-08 19:31:43 -0400

    simplify logging, use json with env var
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,9 @@
-FROM nginx
+FROM nginx:stable-alpine
 
 LABEL maintainer="Erin Schnabel <schnabel@us.ibm.com> (@ebullientworks)"
 
-ENV ETCD_VERSION 2.2.2
-
-RUN apt-get update \
-  && apt-get install -y \
-     curl \
-     procps \
-     wget \
-  && rm -rf /var/lib/apt/lists/* \
-  && wget https://github.com/coreos/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz -q \
-  && tar xzf etcd-v${ETCD_VERSION}-linux-amd64.tar.gz etcd-v${ETCD_VERSION}-linux-amd64/etcdctl --strip-components=1 \
-  && rm etcd-v${ETCD_VERSION}-linux-amd64.tar.gz \
-  && mv etcdctl /usr/local/bin/etcdctl
-
 COPY docker/nginx.conf        /etc/nginx/nginx.conf
-COPY docker/nginx-common.conf /etc/nginx/nginx-common.conf
-COPY docker/nginx-nolog.conf  /etc/nginx/nginx-nolog.conf
 COPY docker/startup.sh        /opt/startup.sh
-COPY docker/forwarder.conf    /opt/forwarder.conf
 
 ADD app/dist/ /opt/www/public
 
--- docker/startup.sh
+++ docker/startup.sh
@@ -1,40 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-# Configure our link to etcd based on shared volume with secret
-if [ ! -z "$ETCD_SECRET" ]; then
-  echo "Configuring for secure etcd"
-  . /data/primordial/setup.etcd.sh /data/primordial $ETCD_SECRET
-else
-  echo "Using local env for any etcd"
-fi
-
-if [ "$ETCDCTL_ENDPOINT" != "" ]; then
-  echo Setting up etcd...
-  echo "** Testing etcd is accessible"
-  etcdctl --debug ls
-  RC=$?
-
-  while [ $RC -ne 0 ]; do
-    sleep 15
-    # recheck condition
-    echo "** Re-testing etcd connection"
-    etcdctl --debug ls
-    RC=$?
-  done
-  echo "etcdctl returned sucessfully, continuing"
-
-  export LOGSTASH_ENDPOINT=$(etcdctl get /logstash/endpoint)
-else
-  #local environment, no logstash to use.
-  echo -n "Making sure application has been built .. "
-  ANGULAR=$(grep -r angular /opt/www/public)
-  if [ -z "$ANGULAR" ]; then
+if [ "${GAMEON_MODE}" == "development" ]
+then
+  if ! grep -q -r angular /opt/www/public
+  then
     echo "The application was not built before packaging."
     exit 1
-  else
-    echo ".. all is well."
   fi
+
+  # turn off sendfile for local development
+  sed -i -e "s/sendfile: .*$/sendfile: off/" /etc/nginx/nginx.conf
+else
+  # turn on sendfile
+  sed -i -e "s/sendfile: .*$/sendfile: on/" /etc/nginx/nginx.conf
 fi
 
-echo Launching nginx with  logging to standard out
-nginx -c /etc/nginx/nginx-nolog.conf
+if [ "${GAMEON_LOG_FORMAT}" == "json" ]
+then
+  sed -i -e "s/access\.log .*$/access.log json_combined;/" /etc/nginx/nginx.conf
+else
+  sed -i -e "s/access\.log .*$/access.log combined;/" /etc/nginx/nginx.conf
+fi
+
+nginx
